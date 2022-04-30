ad447ffc6d2ed2b9b2baa87541921a837cc8f013
Daniel James <daniel@thzinc.com>
2022-04-30 16:46:18 -0700

    (+semver: feature) Combine exporters
    ...because the next sensor (SGP30) needs temp/humidity data to set baselines

--- sensor-exporter/cmd/main.go
+++ sensor-exporter/cmd/main.go
@@ -4,7 +4,8 @@ import (
 	"context"
 	"fmt"
 	"net/http"
-	"plantower-pms5003-exporter/pms5003"
+	"sensor-exporter/aht20"
+	"sensor-exporter/pms5003"
 	"strings"
 	"time"
 
@@ -20,17 +21,35 @@ import (
 
 const (
 	DefaultMetricsPort      int           = 9100
-	DefaultPortName         string        = "/dev/ttyAMA0"
+	DefaultPMS5003PortName  string        = "/dev/ttyAMA0"
+	DefaultAHT20I2CAddr     uint8         = 0x38
+	DefaultAHT20I2CBus      int           = 1
 	DefaultReconnectTimeout time.Duration = 1 * time.Second
 )
 
 var (
+	aht_received_packets = promauto.NewCounter(
+		prometheus.CounterOpts{
+			Name: "aht_received_packets",
+		},
+	)
+	aht_relative_humidity = promauto.NewGauge(
+		prometheus.GaugeOpts{
+			Name: "aht_relative_humidity",
+			Help: "Percentage of relative humidity",
+		},
+	)
+	aht_temperature = promauto.NewGauge(
+		prometheus.GaugeOpts{
+			Name: "aht_temperature",
+			Help: "Temperature in degrees Celsius",
+		},
+	)
 	pms_received_packets = promauto.NewCounter(
 		prometheus.CounterOpts{
 			Name: "pms_received_packets",
 		},
 	)
-
 	pms_particulate_matter_standard = promauto.NewGaugeVec(
 		prometheus.GaugeOpts{
 			Name: "pms_particulate_matter_standard",
@@ -38,7 +57,6 @@ var (
 		},
 		[]string{"microns"},
 	)
-
 	pms_particulate_matter_environmental = promauto.NewGaugeVec(
 		prometheus.GaugeOpts{
 			Name: "pms_particulate_matter_environmental",
@@ -46,7 +64,6 @@ var (
 		},
 		[]string{"microns"},
 	)
-
 	pms_particle_counts = promauto.NewGaugeVec(
 		prometheus.GaugeOpts{
 			Name: "pms_particle_counts",
@@ -84,15 +101,15 @@ var (
 				return metricServer.Close()
 			})
 
-			sensor := pms5003.NewSensor(settings.PortName, settings.ReconnectTimeout)
-			log.Info("starting sensor",
-				"sensor", sensor)
-			group.Go(sensor.Start(group.Context()))
+			particulateSensor := pms5003.NewSensor(settings.PMSPortName, settings.ReconnectTimeout)
+			log.Info("starting particulate sensor",
+				"sensor", particulateSensor)
+			group.Go(particulateSensor.Start(group.Context()))
 			group.Go(func() error {
 				for {
 					log.Debug("waiting for reading...")
 					select {
-					case reading, ok := <-sensor.Readings():
+					case reading, ok := <-particulateSensor.Readings():
 						if !ok {
 							log.Debug("readings channel closed")
 							return nil
@@ -120,6 +137,32 @@ var (
 				}
 			})
 
+			tempHumiditySensor := aht20.NewSensor(settings.AHT20I2CAddr, settings.AHT20I2CBus, settings.ReconnectTimeout)
+			log.Info("starting temperature and humidity sensor",
+				"sensor", tempHumiditySensor)
+			group.Go(tempHumiditySensor.Start(group.Context()))
+			group.Go(func() error {
+				for {
+					log.Debug("waiting for reading...")
+					select {
+					case reading, ok := <-tempHumiditySensor.Readings():
+						if !ok {
+							log.Debug("readings channel closed")
+							return nil
+						}
+
+						log.Debug("received reading",
+							"reading", reading)
+
+						aht_received_packets.Inc()
+						aht_relative_humidity.Set(float64(reading.Humidity))
+						aht_temperature.Set(float64(reading.Temperature))
+					case <-group.Context().Done():
+						return nil
+					}
+				}
+			})
+
 			return group.Wait()
 		},
 	}
@@ -128,16 +171,20 @@ var (
 // Settings defines the configured settings for the exporter
 type Settings struct {
 	MetricsPort      int           `mapstructure:"metrics-port"`
-	PortName         string        `mapstructure:"port"`
 	ReconnectTimeout time.Duration `mapstructure:"reconnect-timeout"`
+	PMSPortName      string        `mapstructure:"pms5003-port"`
+	AHT20I2CAddr     uint8         `mapstructure:"aht20-i2c-addr"`
+	AHT20I2CBus      int           `mapstructure:"aht20-i2c-bus"`
 }
 
 func init() {
 	rootCmd.Flags().Int("metrics-port", DefaultMetricsPort, "Port on which to host Prometheus metrics")
-	rootCmd.Flags().String("port", DefaultPortName, "Path or name of block device through which to read from the sensor")
 	rootCmd.Flags().Duration("reconnect-timeout", DefaultReconnectTimeout, "Duration to wait before attempting to reconnect to the sensor after a failure")
+	rootCmd.Flags().String("pms5003-port", DefaultPMS5003PortName, "Path or name of block device through which to read from the Plantower PMS5003 sensor")
+	rootCmd.Flags().Uint8("aht20-i2c-addr", DefaultAHT20I2CAddr, "I2C address of the Asair AHT20 sensor")
+	rootCmd.Flags().Int("aht20-i2c-bus", DefaultAHT20I2CBus, "I2C bus to which the Asair ATH20 sensor is attached")
 
-	viper.SetEnvPrefix("PMS5003")
+	viper.SetEnvPrefix("EXPORTER")
 	replacer := strings.NewReplacer("-", "_")
 	viper.SetEnvKeyReplacer(replacer)
 	viper.AutomaticEnv()
--- sensor-exporter/go.mod
+++ sensor-exporter/go.mod
@@ -1,15 +1,14 @@
-module asair-aht20-exporter
+module sensor-exporter
 
 go 1.18
 
 require (
-	github.com/pkg/errors v0.9.1
 	github.com/prometheus/client_golang v1.12.1
-	github.com/syncromatics/go-kit/v2 v2.3.1
-	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c
+	github.com/tarm/serial v0.0.0-20180830185346-98f6abe2eb07
 )
 
 require (
+	github.com/BurntSushi/toml v0.3.1 // indirect
 	github.com/d2r2/go-logger v0.0.0-20210606094344-60e9d1233e22 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/fsnotify/fsnotify v1.5.1 // indirect
@@ -24,33 +23,35 @@ require (
 	github.com/spf13/jwalterweatherman v1.1.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/subosito/gotenv v1.2.0 // indirect
-	golang.org/x/text v0.3.7 // indirect
-	gopkg.in/ini.v1 v1.66.4 // indirect
-	gopkg.in/yaml.v2 v2.4.0 // indirect
-	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
-)
-
-require (
-	github.com/BurntSushi/toml v0.3.1 // indirect
-	github.com/beorn7/perks v1.0.1 // indirect
-	github.com/cespare/xxhash/v2 v2.1.2 // indirect
-	github.com/d2r2/go-i2c v0.0.0-20191123181816-73a8a799d6bc
-	github.com/golang/protobuf v1.5.2 // indirect
-	github.com/matttproud/golang_protobuf_extensions v1.0.1 // indirect
-	github.com/prometheus/client_model v0.2.0 // indirect
-	github.com/prometheus/common v0.32.1 // indirect
-	github.com/prometheus/procfs v0.7.3 // indirect
-	github.com/spf13/cobra v1.4.0
-	github.com/spf13/viper v1.11.0
 	go.uber.org/atomic v1.5.0 // indirect
 	go.uber.org/multierr v1.3.0 // indirect
 	go.uber.org/tools v0.0.0-20190618225709-2cfd321de3ee // indirect
 	go.uber.org/zap v1.13.0 // indirect
 	golang.org/x/lint v0.0.0-20201208152925-83fdc39ff7b5 // indirect
 	golang.org/x/mod v0.4.1 // indirect
-	golang.org/x/sys v0.0.0-20220412211240-33da011f77ad // indirect
+	golang.org/x/text v0.3.7 // indirect
 	golang.org/x/tools v0.1.0 // indirect
 	golang.org/x/xerrors v0.0.0-20220411194840-2f41105eb62f // indirect
-	google.golang.org/protobuf v1.28.0 // indirect
+	gopkg.in/ini.v1 v1.66.4 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
 	honnef.co/go/tools v0.0.1-2020.1.4 // indirect
+)
+
+require (
+	github.com/beorn7/perks v1.0.1 // indirect
+	github.com/cespare/xxhash/v2 v2.1.2 // indirect
+	github.com/d2r2/go-i2c v0.0.0-20191123181816-73a8a799d6bc
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/matttproud/golang_protobuf_extensions v1.0.1 // indirect
+	github.com/pkg/errors v0.9.1
+	github.com/prometheus/client_model v0.2.0 // indirect
+	github.com/prometheus/common v0.32.1 // indirect
+	github.com/prometheus/procfs v0.7.3 // indirect
+	github.com/spf13/cobra v1.4.0
+	github.com/spf13/viper v1.11.0
+	github.com/syncromatics/go-kit/v2 v2.3.1
+	golang.org/x/sync v0.0.0-20201207232520-09787c993a3a
+	golang.org/x/sys v0.0.0-20220412211240-33da011f77ad // indirect
+	google.golang.org/protobuf v1.28.0 // indirect
 )
