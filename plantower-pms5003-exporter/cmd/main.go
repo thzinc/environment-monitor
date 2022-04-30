39391e0f67397d2ff306f76ebf714116254e832c
Daniel James <daniel@thzinc.com>
2022-04-30 11:48:39 -0700

    (+semver: feature) Use cobra/viper for config

--- plantower-pms5003-exporter/cmd/main.go
+++ plantower-pms5003-exporter/cmd/main.go
@@ -5,11 +5,15 @@ import (
 	"fmt"
 	"net/http"
 	"plantower-pms5003-exporter/pms5003"
+	"strings"
 	"time"
 
+	"github.com/pkg/errors"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promauto"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 	"github.com/syncromatics/go-kit/v2/cmd"
 	"github.com/syncromatics/go-kit/v2/log"
 )
@@ -50,74 +54,98 @@ var (
 		},
 		[]string{"microns_lower_bound"},
 	)
+	rootCmd = cobra.Command{
+		Use:           "plantower-pms5003-exporter",
+		Short:         "start collecting readings from the sensor and host a metrics server",
+		SilenceErrors: true,
+		SilenceUsage:  true,
+		RunE: func(_ *cobra.Command, args []string) error {
+			settings := &Settings{}
+			err := viper.Unmarshal(settings)
+			if err != nil {
+				return errors.Wrap(err, "failed to parse settings")
+			}
+
+			group := cmd.NewProcessGroup(context.Background())
+
+			metricServer := http.Server{
+				Addr:    fmt.Sprintf(":%d", settings.MetricsPort),
+				Handler: nil,
+			}
+			log.Info("starting metrics server",
+				"addr", metricServer.Addr)
+			group.Go(func() error {
+				http.Handle("/metrics", promhttp.Handler())
+				return metricServer.ListenAndServe()
+			})
+			group.Go(func() error {
+				<-group.Context().Done()
+				log.Info("stopping metrics server")
+				return metricServer.Close()
+			})
+
+			sensor := pms5003.NewSensor(settings.PortName, settings.ReconnectTimeout)
+			log.Info("starting sensor",
+				"sensor", sensor)
+			group.Go(sensor.Start(group.Context()))
+			group.Go(func() error {
+				for {
+					log.Debug("waiting for reading...")
+					select {
+					case reading, ok := <-sensor.Readings():
+						if !ok {
+							log.Debug("readings channel closed")
+							return nil
+						}
+
+						log.Debug("received reading",
+							"reading", reading)
+
+						pms_received_packets.Inc()
+						pms_particulate_matter_standard.WithLabelValues("1").Set(float64(reading.Pm10Std))
+						pms_particulate_matter_standard.WithLabelValues("2.5").Set(float64(reading.Pm25Std))
+						pms_particulate_matter_standard.WithLabelValues("10").Set(float64(reading.Pm100Std))
+						pms_particulate_matter_environmental.WithLabelValues("1").Set(float64(reading.Pm10Env))
+						pms_particulate_matter_environmental.WithLabelValues("2.5").Set(float64(reading.Pm25Env))
+						pms_particulate_matter_environmental.WithLabelValues("10").Set(float64(reading.Pm100Env))
+						pms_particle_counts.WithLabelValues("0.3").Set(float64(reading.Particles3um))
+						pms_particle_counts.WithLabelValues("0.5").Set(float64(reading.Particles5um))
+						pms_particle_counts.WithLabelValues("1").Set(float64(reading.Particles10um))
+						pms_particle_counts.WithLabelValues("2.5").Set(float64(reading.Particles25um))
+						pms_particle_counts.WithLabelValues("5").Set(float64(reading.Particles50um))
+						pms_particle_counts.WithLabelValues("10").Set(float64(reading.Particles100um))
+					case <-group.Context().Done():
+						return nil
+					}
+				}
+			})
+
+			return group.Wait()
+		},
+	}
 )
 
+// Settings defines the configured settings for the exporter
+type Settings struct {
+	MetricsPort      int           `mapstructure:"metrics-port"`
+	PortName         string        `mapstructure:"port"`
+	ReconnectTimeout time.Duration `mapstructure:"reconnect-timeout"`
+}
+
+func init() {
+	rootCmd.Flags().Int("metrics-port", DefaultMetricsPort, "Port on which to host Prometheus metrics")
+	rootCmd.Flags().String("port", DefaultPortName, "Path or name of block device through which to read from the sensor")
+	rootCmd.Flags().Duration("reconnect-timeout", DefaultReconnectTimeout, "Duration to wait before attempting to reconnect to the sensor after a failure")
+
+	viper.SetEnvPrefix("PMS5003")
+	replacer := strings.NewReplacer("-", "_")
+	viper.SetEnvKeyReplacer(replacer)
+	viper.AutomaticEnv()
+	viper.BindPFlags(rootCmd.Flags())
+}
+
 func main() {
-	group := cmd.NewProcessGroup(context.Background())
-
-	metricServer := http.Server{
-		Addr:    fmt.Sprintf(":%d", DefaultMetricsPort), // TODO: read from settings
-		Handler: nil,
-	}
-	log.Info("starting metrics server",
-		"addr", metricServer.Addr)
-	group.Go(func() error {
-		http.Handle("/metrics", promhttp.Handler())
-		return metricServer.ListenAndServe()
-	})
-	group.Go(func() error {
-		<-group.Context().Done()
-		log.Info("stopping metrics server")
-		return metricServer.Close()
-	})
-
-	reconnectTimeout, err := time.ParseDuration("1s") // TODO: read from settings
-	if err != nil {
-		log.Warn("failed to parse reconnect timeout duration; using default timeout",
-			"err", err,
-			"DefaultReconnectTimeout", DefaultReconnectTimeout)
-		reconnectTimeout = DefaultReconnectTimeout
-	}
-
-	portName := DefaultPortName // TODO: read from settings
-
-	sensor := pms5003.NewSensor(portName, reconnectTimeout)
-	log.Info("starting sensor",
-		"sensor", sensor)
-	group.Go(sensor.Start(group.Context()))
-	group.Go(func() error {
-		for {
-			log.Debug("waiting for reading...")
-			select {
-			case reading, ok := <-sensor.Readings():
-				if !ok {
-					log.Debug("readings channel closed")
-					return nil
-				}
-
-				log.Debug("received reading",
-					"reading", reading)
-
-				pms_received_packets.Inc()
-				pms_particulate_matter_standard.WithLabelValues("1").Set(float64(reading.Pm10Std))
-				pms_particulate_matter_standard.WithLabelValues("2.5").Set(float64(reading.Pm25Std))
-				pms_particulate_matter_standard.WithLabelValues("10").Set(float64(reading.Pm100Std))
-				pms_particulate_matter_environmental.WithLabelValues("1").Set(float64(reading.Pm10Env))
-				pms_particulate_matter_environmental.WithLabelValues("2.5").Set(float64(reading.Pm25Env))
-				pms_particulate_matter_environmental.WithLabelValues("10").Set(float64(reading.Pm100Env))
-				pms_particle_counts.WithLabelValues("0.3").Set(float64(reading.Particles3um))
-				pms_particle_counts.WithLabelValues("0.5").Set(float64(reading.Particles5um))
-				pms_particle_counts.WithLabelValues("1").Set(float64(reading.Particles10um))
-				pms_particle_counts.WithLabelValues("2.5").Set(float64(reading.Particles25um))
-				pms_particle_counts.WithLabelValues("5").Set(float64(reading.Particles50um))
-				pms_particle_counts.WithLabelValues("10").Set(float64(reading.Particles100um))
-			case <-group.Context().Done():
-				return nil
-			}
-		}
-	})
-
-	err = group.Wait()
+	err := rootCmd.Execute()
 	if err != nil {
 		log.Fatal("failed to terminate cleanly",
 			"err", err)
--- plantower-pms5003-exporter/go.mod
+++ plantower-pms5003-exporter/go.mod
@@ -9,14 +9,30 @@ require (
 
 require (
 	github.com/BurntSushi/toml v0.3.1 // indirect
+	github.com/fsnotify/fsnotify v1.5.1 // indirect
+	github.com/hashicorp/hcl v1.0.0 // indirect
+	github.com/inconshreveable/mousetrap v1.0.0 // indirect
+	github.com/magiconair/properties v1.8.6 // indirect
+	github.com/mitchellh/mapstructure v1.4.3 // indirect
+	github.com/pelletier/go-toml v1.9.4 // indirect
+	github.com/pelletier/go-toml/v2 v2.0.0-beta.8 // indirect
+	github.com/spf13/afero v1.8.2 // indirect
+	github.com/spf13/cast v1.4.1 // indirect
+	github.com/spf13/jwalterweatherman v1.1.0 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/subosito/gotenv v1.2.0 // indirect
 	go.uber.org/atomic v1.5.0 // indirect
 	go.uber.org/multierr v1.3.0 // indirect
 	go.uber.org/tools v0.0.0-20190618225709-2cfd321de3ee // indirect
 	go.uber.org/zap v1.13.0 // indirect
-	golang.org/x/lint v0.0.0-20200302205851-738671d3881b // indirect
-	golang.org/x/mod v0.3.0 // indirect
-	golang.org/x/tools v0.0.0-20200825202427-b303f430e36d // indirect
-	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
+	golang.org/x/lint v0.0.0-20201208152925-83fdc39ff7b5 // indirect
+	golang.org/x/mod v0.4.1 // indirect
+	golang.org/x/text v0.3.7 // indirect
+	golang.org/x/tools v0.1.0 // indirect
+	golang.org/x/xerrors v0.0.0-20220411194840-2f41105eb62f // indirect
+	gopkg.in/ini.v1 v1.66.4 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
 	honnef.co/go/tools v0.0.1-2020.1.4 // indirect
 )
 
@@ -29,8 +45,10 @@ require (
 	github.com/prometheus/client_model v0.2.0 // indirect
 	github.com/prometheus/common v0.32.1 // indirect
 	github.com/prometheus/procfs v0.7.3 // indirect
+	github.com/spf13/cobra v1.4.0
+	github.com/spf13/viper v1.11.0
 	github.com/syncromatics/go-kit/v2 v2.3.1
 	golang.org/x/sync v0.0.0-20201207232520-09787c993a3a
-	golang.org/x/sys v0.0.0-20220114195835-da31bd327af9 // indirect
-	google.golang.org/protobuf v1.26.0 // indirect
+	golang.org/x/sys v0.0.0-20220412211240-33da011f77ad // indirect
+	google.golang.org/protobuf v1.28.0 // indirect
 )
