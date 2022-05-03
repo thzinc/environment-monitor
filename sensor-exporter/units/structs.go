d4851c46afad95d64a6369b1fb0cc218478de2c4
Daniel James <daniel@thzinc.com>
2022-05-02 20:38:00 -0700

    (+semver: fix) Fix absolute humidity calculation

--- sensor-exporter/units/structs.go
+++ sensor-exporter/units/structs.go
@@ -7,11 +7,11 @@ type Celsius float64
 type GramsPerCubicMeter float64
 
 func AbsoluteHumidity(temperature Celsius, relativeHumidity RelativeHumidity) GramsPerCubicMeter {
-	rh := float64(relativeHumidity)
-	c := float64(temperature)
-	numerator := rh * 6.112 * math.Exp((17.62*c)/(243.12+c))
-	denominator := 273.15 + c
+	// Adapted from https://github.com/skgrange/threadr/blob/fd42380883133fe7a47c479e778afe644a507334/R/absolute_humidity.R
+	rh := float64(relativeHumidity) * 100
+	t := float64(temperature)
+	h := (6.112 * math.Exp((17.67*t)/(t+243.5)) * rh * 2.1674) / (273.15 + t)
 
-	humidity := GramsPerCubicMeter(216.7 * (numerator / denominator))
+	humidity := GramsPerCubicMeter(h)
 	return humidity
 }
